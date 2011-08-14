073eb312751c9df98b8b38ef7bf77b90c8c08de2
Denis Orlikhin <denis@ostrovok.ru>
2011-08-15 02:13:21 +0400

    save&display

--- dinner/views.py
+++ dinner/views.py
@@ -22,17 +22,27 @@ def reserve(request):
         order = m.Order.objects.get(user=request.user, menu=menu)
 
         for key, value in request.POST.items():
-            if key.startswith('dish#') and value and int(value):
+            if key.startswith('dish#') and value:
                 dish_id = int(key[5:])
-                m.OrderDayItem(order=order, dish_id=dish_id, count=int(value)).save()
+                try:
+                    item = m.OrderDayItem.objects.get(order=order, dish__id=dish_id)
+                    item.count = int(value)
+                    item.save()
+                except m.OrderDayItem.DoesNotExist:
+                    if int(value):
+                        m.OrderDayItem(order=order, dish_id=dish_id, count=int(value)).save()
 
         return HttpResponseRedirect(request.get_full_path())
 
     menu = m.Menu.objects.get(week__gt = datetime.now() - timedelta(2))
 
     order = m.Order.objects.get_or_create(user=request.user, menu=menu)[0]
-    #items = m.OrderDayItem.objects.filter(order=order, dish__day__week=menu).select_related().order_by('dish__pk')
+    ordered_items = dict(m.OrderDayItem.objects.filter(order=order, dish__day__week=menu).values_list('dish__pk', 'count'))
+
     dishes = m.Dish.objects.filter(day__week=menu).select_related().order_by('pk')
+
+    for d in dishes:
+        d.count = ordered_items.get(d.pk, 0)
 
     dishes_by_group = _group_by_materialize(groupby(dishes, lambda i: (i.day, i.group)))
     dishes_by_group_by_day = _group_by_materialize(groupby(dishes_by_group, lambda i: i[0][0]))
