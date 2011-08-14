cffffd5173b90cc01d787cf8c946ea857e859d5b
Denis Orlikhin <denis@ostrovok.ru>
2011-08-15 01:54:00 +0400

    menu saving

--- dinner/views.py
+++ dinner/views.py
@@ -1,17 +1,34 @@
 # coding: utf-8
 from pprint import pformat
 from django.contrib.auth.decorators import login_required
+from django.db.transaction import commit_on_success
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
 import models as m
 from datetime import datetime
 from itertools import groupby
+from datetime import timedelta
 
 def _group_by_materialize(seq):
     return [(k, list(v)) for k, v in seq]
 
 @login_required
+@commit_on_success
 def reserve(request):
-    menu = m.Menu.objects.get(week__gt = datetime.now())
+
+    if request.method == 'POST':
+        menu = m.Menu.objects.get(pk=request.POST['menu'])
+        order = m.Order.objects.get(user=request.user, menu=menu)
+
+        for key, value in request.POST.items():
+            if key.startswith('dish#') and value and int(value):
+                dish_id = int(key[5:])
+                m.OrderDayItem(order=order, dish_id=dish_id, count=int(value)).save()
+
+        return HttpResponseRedirect(request.get_full_path())
+
+    menu = m.Menu.objects.get(week__gt = datetime.now() - timedelta(2))
 
     order = m.Order.objects.get_or_create(user=request.user, menu=menu)[0]
     #items = m.OrderDayItem.objects.filter(order=order, dish__day__week=menu).select_related().order_by('dish__pk')
@@ -19,9 +36,6 @@ def reserve(request):
 
     dishes_by_group = _group_by_materialize(groupby(dishes, lambda i: (i.day, i.group)))
     dishes_by_group_by_day = _group_by_materialize(groupby(dishes_by_group, lambda i: i[0][0]))
-    #dishes_by_group_by_day = [(key, list(items)) for key, items in groupby(dishes_by_group, lambda i: next(i)[2].pk)]
-    #raise Exception(pformat(dishes_by_group[1][2].pk))
-    #raise Exception(pformat(list(next(next(dishes_by_group_by_day)[1])[1])))
 
 
     return direct_to_template(request, 'dinner/reserve.html', {
