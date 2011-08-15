f1e6cffcf5aa39d661a1f15ee3e4084f5f44a879
Denis Orlikhin <denis@ostrovok.ru>
2011-08-15 04:00:58 +0400

    cleanup

--- urls.py
+++ urls.py
@@ -7,13 +7,6 @@ import dinner.urls
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'staff.views.home', name='home'),
-    # url(r'^staff/', include('staff.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^accounts/', include('socialauth.urls')),
