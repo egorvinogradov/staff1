fde2eb0a40da890e9b49fabcf4cbf45b9bb439f8
Denis Orlikhin <denis@ostrovok.ru>
2011-08-18 21:07:54 +0400

    password

--- settings/django.py
+++ settings/django.py
@@ -15,9 +15,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'staff',                      # Or path to database file if using sqlite3.
-        'USER': 'denis',                       # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': 'staff',                       # Not used with sqlite3.
+        'PASSWORD': 'VgcVS/6DFnDP48yfwcygUwxISGHtK',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
