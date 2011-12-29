9e19be706dd0fe4e9c9465094cf562d1bd029728
Yakov Borevich <jjay@ostrovok.ru>
2011-12-29 14:33:18 +0400

    Support for fucken fusion changes

--- dinner/admin.py
+++ dinner/admin.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import re
 from pprint import pformat
 from django.contrib import admin
 from django.contrib.auth.models import User
@@ -168,7 +169,7 @@ class MenuAdmin(admin.ModelAdmin):
         week = menu.week = form.cleaned_data['week'] = _get_weekobj(week_date.date())
         super(MenuAdmin, self).save_model(request, menu, form, change)
 
-        days_map = dict((day_name.capitalize() + ' 3', day_num) for day_num, day_name in enumerate(u'понедельник вторник среда четверг пятница суббота воскресенье'.split(' ')))
+        days_map = dict((day_name.capitalize(), day_num) for day_num, day_name in enumerate(u'понедельник вторник среда четверг пятница суббота воскресенье'.split(' ')))
 
         data = []
 
@@ -183,23 +184,24 @@ class MenuAdmin(admin.ModelAdmin):
 
             if not any(row):
                 continue
-            elif row[0].capitalize() in days_map:
+            elif re.sub("^['\"]|[\s\"'\d]*$", "", row[0]).capitalize() in days_map:
                 if not (day is None):
                     #raise Exception(pformat(data).decode('unicode-escape'))
                     __save_data(day, data, provider)
                     data = []
-
-                day = m.Day.objects.get_or_create(day=days_map[row[0].capitalize()], week=week)[0]
+                day_name = re.sub("^['\"]|[\s\"'\d]*$", "", row[0]).capitalize()
+                day = m.Day.objects.get_or_create(day=days_map[day_name], week=week)[0]
             elif len(row)==1 and row[0]:
                 group = row[0]
             elif len(row)==2 and not row[0] and row[1]:
                 group = row[1]
-            elif len(row)==3 and row[0]==u'выход':
+            elif len(row)==5 and row[0] ==u'выход' or row[0]==u"":
                 group = row[1]
-            else:
+            elif len(row)==5 and re.match("^\d+", row[0]):
                 if len(row) < 3:
                     raise Exception("Unexpected row: " + pformat(row).decode('unicode-escape'))
-                weight, title, price = row
+                print 'row', pformat(row).decode('unicode-escape')
+                weight, title, price, amount, total = row
 
                 if len(rows[i])==1 and rows[i][0][0] == '(':
                     title += "\n" + rows[i][0].decode('utf-8')
@@ -210,6 +212,8 @@ class MenuAdmin(admin.ModelAdmin):
                 if not group:
                     raise Exception('no group found yet line#{0}'.format(i))
                 data.append((group, weight, title, price, index))
+            else:
+                continue
             
         #raise Exception(pformat(data).decode('unicode-escape'))
         __save_data(day, data, provider)
