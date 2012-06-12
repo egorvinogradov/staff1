25ce5e78c954893af7a00a843e44ba947e1ced12
Sergei Surovtsev <cyberwizard.ru@gmail.com>
2012-06-13 02:59:26 +0400

    old db

--- settings/common.py
+++ settings/common.py
@@ -11,15 +11,29 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'food', # Or path to database file if using sqlite3.
+#        'USER': 'postgres', # Not used with sqlite3.
+#        'PASSWORD': 'suprpass616', # Not used with sqlite3.
+#        'HOST': '78.46.187.179', # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'food', # Or path to database file if using sqlite3.
-        'USER': 'postgres', # Not used with sqlite3.
-        'PASSWORD': 'suprpass616', # Not used with sqlite3.
-        'HOST': '78.46.187.179', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'staff_test_3',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                       # Not used with sqlite3.
+        'PASSWORD': '2minutes2midnight',                  # Not used with sqlite3.
+        'HOST': 'dev4-db.ostrovok.ru',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+            'autocommit': False,
+            }
     }
 }
 
