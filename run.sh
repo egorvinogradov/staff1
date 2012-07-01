89871e51a5d8ec79601f1f76a0049abf150fb236
Sergei Surovtsev <cyberwizard.ru@gmail.com>
2012-07-01 22:40:44 +0400

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#source /usr/local/bin/virtualenvwrapper.sh
+#workon food
+
 set -e
 LOGFILE=logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
@@ -6,11 +10,7 @@ NUM_WORKERS=3
 
 # user/group to run as
 USER=cwiz
-GROUP=1000
-
-source /usr/local/bin/virtualenvwrapper.sh
-workon food
 
 test -d $LOGDIR || mkdir -p $LOGDIR
 
-exec python manage.py run_gunicorn -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
+exec python manage.py run_gunicorn -w $NUM_WORKERS --user=$USER --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
