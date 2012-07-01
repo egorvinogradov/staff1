1bae10141f72ae6e56d1ff69d6db3573ab9c70ce
Sergei Surovtsev <cyberwizard.ru@gmail.com>
2012-07-01 22:49:59 +0400

    files

--- __deploy/run.sh
+++ __deploy/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ..
+
 source /usr/local/bin/virtualenvwrapper.sh
 workon food
 pip install -r requirements.txt
@@ -13,5 +15,8 @@ NUM_WORKERS=3
 USER=cwiz
 
 test -d $LOGDIR || mkdir -p $LOGDIR
+
+try cp upstart.conf /etc/init/food.conf
+try ln -s /lib/init/upstart-job /etc/init.d/food
 
 exec python manage.py run_gunicorn -w $NUM_WORKERS --user=$USER --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE
